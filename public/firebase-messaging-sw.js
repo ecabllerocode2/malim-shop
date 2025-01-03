54ca97d3ad726c235dc9182fbd1f102c310c2fee
ecabllerocode2 <caballerogarcia.ederaaron@gmail.com>
2025-01-03 03:13:36 +0000

    ultimo commit

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,9 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
+
+const { data } = require("autoprefixer");
+
 // Replace 10.13.2 with latest version of the Firebase JS SDK.
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
@@ -9,30 +12,45 @@ importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-com
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
-    apiKey: "AIzaSyAcPmOLCEeL5sRenwhtTWCIBawWNcnD4Ls",
-    authDomain: "malim-app.firebaseapp.com",
-    projectId: "malim-app",
-    storageBucket: "malim-app.firebasestorage.app",
-    messagingSenderId: "953747301080",
-    appId: "1:953747301080:web:d3cfd18e9be012bb822dad",
-    measurementId: "G-9DD5YEX28R"
-  });
+  apiKey: "AIzaSyAcPmOLCEeL5sRenwhtTWCIBawWNcnD4Ls",
+  authDomain: "malim-app.firebaseapp.com",
+  projectId: "malim-app",
+  storageBucket: "malim-app.firebasestorage.app",
+  messagingSenderId: "953747301080",
+  appId: "1:953747301080:web:d3cfd18e9be012bb822dad",
+  measurementId: "G-9DD5YEX28R"
+});
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    console.log(
-      '[firebase-messaging-sw.js] Received background message ' +
-      payload
-    );
-    // Customize notification here
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-      icon: payload.notification.image,
-    };
-  
-    self.registration.showNotification(notificationTitle, notificationOptions);
-  });
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ' +
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image,
+    url: payload.data?.click_action
+
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  const productUrl = event.notification.data?.click_action;
+  event.waitUntil(
+    clients.openWindow(productUrl).then((windowClient) => {
+      if (windowClient) {
+        windowClient.focus();
+      }
+    })
+  );
+});
